6c85296a5e96bd2b3a9c2cb8612a2b87957021aa
Andrea Sánchez Tapia <katori@gmail.com>
2020-04-20 11:54:36 -0700

    check string OK will remove 04_checked names.csv

--- scripts/01_name_search.R
+++ scripts/01_name_search.R
@@ -3,36 +3,34 @@ library(dplyr)
 library(purrr)
 devtools::load_all("../andre_rocc/")
 
+cerrado_all <- search_flora(domain = "Cerrado")
 cerrado_endemics <- search_flora(domain = "Cerrado",
                                  endemism = TRUE)
-cerrado_all <- search_flora(domain = "Cerrado")
 write_csv(cerrado_endemics, "./output/01_all_names.csv")
 
 # Check names
-check_names_df <- purrr::mapdf(cerrado_endemics$scientificName,
+check_names_df <- purrr::map_df(cerrado_endemics$scientificName,
                           ~rocc::check_string(scientificName = .))
 write_csv(check_names_df, "./output/02_check_string.csv")
 
 table(check_names_df$scientificName_status)
-# I did a semi-thourough checking of the results and reported them to Sara.
-# Flora output has authors so we have to assume that check_string will get lots of authors all the time.
 
+# I did a semi-thorough checking of the results and reported them to Sara.
+# Flora output has authors so we have to assume that check_string will get lots of authors all the time.
+check_names_df %>% filter(scientificName_status == "forma") %>% View()
+check_names_df %>% filter(scientificName_status == "hybrid_species") %>% View()
+check_names_df %>% filter(scientificName_status == "subspecies") %>% View()
+check_names_df %>% filter(scientificName_status == "variety") %>% View()
+check_names_df %>% filter(scientificName_status == "name_w_authors") %>% head() %>% View()
+check_names_df %>% filter(scientificName_status == "indet") %>% View()
 # ikinci kez para terminar de limpiar
 check_names_iki <- purrr::map_df(check_names_df$scientificName_new,
                           ~rocc::check_string(scientificName = .))
 write_csv(check_names_iki, "./output/03_double_check_string.csv")
+table(check_names_iki$scientificName_status)
+check_names_iki %>% filter(scientificName_status == "hybrid_species") %>% View()
+check_names_iki %>% filter(scientificName_status == "indet") %>% View()
+check_names_iki %>% filter(scientificName_status == "name_w_non_ascii") %>% View()
+check_names_iki %>% filter(scientificName_status == "species_nova") %>% View()
 
-#So indet is catching genera and flora also returns families so at least we have to filter that - I'll just do it the third time to have something.
-check_names <- check_names_iki %>%
-  filter(!scientificName_status %in% c("indet", "family_as_genus"))
-table(check_names$scientificName_status)
-
-check_names_üç <- purrr::map_df(check_names$scientificName_new,
-                                 ~rocc::check_string(scientificName = .))
-write_csv(check_names_üç, "./output/04_checked_names.csv")
-table(check_names_üç$scientificName_status)
-# paro aqui para resolver el resto - hay cosas que persisten y ya lo sabía
-
-
-#check taxon
-rocc::check_taxon(check_names_df$scientificName_new)
+#no more need for the third check because everything that was not possibly ok has no way to resolve
