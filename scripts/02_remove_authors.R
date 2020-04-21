3710f65be0784b564958eccb518d4853a7a8a480
Andrea Sánchez Tapia <katori@gmail.com>
2020-04-20 18:48:18 -0700

    remove then check returns no errors either. won't run again

--- scripts/02_remove_authors.R
+++ scripts/02_remove_authors.R
@@ -1,27 +1,30 @@
 library(magrittr)
-devtools::load_all("../andre_rocc/")
 library(dplyr)
 library(purrr)
+devtools::load_all("../andre_rocc/")
 #testing only the output of remove_authors
 cerrado_endemics <- readr::read_csv("./output/01_all_names.csv")
 remove_authors <- cerrado_endemics$scientificName %>%
   purrr::map(., ~flora::remove.authors(taxon = .))
 #names(remove_authors) <- cerrado_endemics$scientificName
 test <- unlist(remove_authors)
-head(test)#it's a named vector
-final <- data.frame(names_original = names(test), names_wo_authors = test)
+head(test)
+
+final <- data.frame(names_original = cerrado_endemics$scientificName,
+                    names_wo_authors = test)
+head(final)
 test_remove <- final %>%
   mutate(
   Genus = stringr::word(names_wo_authors, 1),
   Epithet = stringr::word(names_wo_authors, 2),
   Anything_else = stringr::word(names_wo_authors, 3)
-  ) %>% mutate(empty = is.na(Anything_else))
+  ) %>%
+  mutate(empty = is.na(Anything_else))
 test_remove %>% count(empty)
 test_remove_summary <- test_remove %>% filter(empty == FALSE)
 #there are vars, subsp and f. we knew that
 #tira de todas as var?
-  test_remove_summary %>%
-  filter(Anything_else %in% c("var.")) %>% View()
+
   View(test_remove_summary)
 #APARENTEMENTE SIM, NAO PRECISA FAZER ANTES VAR E DEPOIS R_A
 
@@ -42,7 +45,7 @@ nrow(dotf)
 #var e subsp. tira bem os autores
 
 readr::write_csv(test_remove_summary, "./output/05_test_remove_authors.csv")
-
+table(test_remove_summary)
 
 errors <- test_remove_summary %>%
   filter(!Anything_else %in% c("var.", "f.", "subsp."))
@@ -53,5 +56,6 @@ check %<>% rename(names_wo_authors = scientificName)
 resumen <- dplyr::left_join(errors, check)
 names(resumen)
 resumen %<>% select(names_original, names_wo_authors, scientificName_status, scientificName_new)
-names(resumen)
+table(resumen$scientificName_status)
+View(resumen)
 readr::write_csv(resumen, "07_remove_then_check_string.csv")
